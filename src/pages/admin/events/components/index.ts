1aedc32ef17c2ab5cbd5398b6358857eb089f4cc
Nguyễn Quang Huy <155792250+HuyNguyen2k4@users.noreply.github.com>
2026-04-10 08:00:32 +0700

    Update index.ts

--- src/pages/admin/events/components/index.ts
+++ src/pages/admin/events/components/index.ts
@@ -7,6 +7,5 @@ export { TicketCatalogList } from './TicketCatalogList';
 export { TicketCatalogFormDialog } from './TicketCatalogFormDialog';
 export { MapPickerModal } from './MapPickerModal';
 export { EventTimelineList } from './eventTimeline/EventTimelineList';
-export { EventTimelineFormDialog } from './eventTimeline/EventTimelineFormDialog';
 export { EventPointList } from './eventPoint/EventPointList';
 export { EventPointTagList } from './eventPoint/EventPointTagList';
